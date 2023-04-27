88d7374ae91af1e438e5b4f5fed2a3a6e2fd776a
chrismdating1206@gmail.com <christian.dating12>
2023-04-27 21:34:36 +0800

    first commit

--- Program 4.py
+++ Program 4.py
@@ -4,3 +4,11 @@
 # integers.txt that contains 20 integers. The first output file will be named double.txt containing the square
 # of all even integers found in integers.txt and the second file will be named triple.txt containing the cube of
 # all odd numbers found in the integers.txt.
+
+input("\n\033[94mPress Any Key to Start...")
+print("\033[90m=" * 80)
+
+# Intro
+import pyfiglet
+greet = "GOOD DAY!"
+print("\033[92m" + pyfiglet.figlet_format(greet, font = "Thin"))
