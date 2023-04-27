31fecda43d110337823bb6679c193523a36b85f8
chrismdating1206@gmail.com <christian.dating12>
2023-04-27 21:31:46 +0800

    first commit

---
+++ Program 4.py
@@ -0,0 +1,6 @@
+# Christian M. Dating | BSCPE 1-5
+
+# Write a method in python that will create two separate text files after reading the source text files named
+# integers.txt that contains 20 integers. The first output file will be named double.txt containing the square
+# of all even integers found in integers.txt and the second file will be named triple.txt containing the cube of
+# all odd numbers found in the integers.txt.
